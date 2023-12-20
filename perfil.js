e72aea6feb5bb91fe1195d30b25c231916f43914
pTrexx <146021693+pTrexx@users.noreply.github.com>
2023-12-20 17:05:38 -0300

    Add files via upload

---
+++ perfil.js
@@ -0,0 +1,23 @@
+function calcularprojeto(){
+    var tipoproj = document.getElementById('tipoprojeto').value;
+    var horaproj = document.getElementById('horasprojeto').value
+
+    var precohora;
+    switch(tipoproj){
+        case 'Website':
+            precohora = 50;
+            break;
+        case 'LandingPage':
+            precohora = 40;
+            break;
+        case 'WebApp':
+            precohora = 60;
+            break;
+        default:
+            precohora = 25;
+            break   
+    }
+
+    var precototal = precohora*horaproj
+    document.getElementById('preço').innerHTML = "Preço do Projeto: R$"+ precototal
+}
